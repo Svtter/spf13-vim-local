3039cfeb9800db44a4c076c8848ca78313519a8d
svtter <svtter@qq.com>
2016-04-13 11:47:15 +0800

    update

---
+++ cp-to.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ~/.vimrc.local ~/.spf13-vim-local/.vimrc.local
+cp ~/.vimrc.bundles.local ~/.spf13-vim-local/.vimrc.bundles.local
+cp ~/.vimrc.before.local ~/.spf13-vim-local/.vimrc.before.local
